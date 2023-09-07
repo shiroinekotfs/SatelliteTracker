0fbd05e0472303f5b1cbdf7f374003409d7232e0
takinekotfs <systakineko.tfs@gmail.com>
2023-09-07 21:09:09 +0700

    Update webpack.prod.js

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,5 +1,7 @@
 const path = require("path");
-const { merge } = require("webpack-merge");
+const {
+    merge
+} = require("webpack-merge");
 
 const basePath = `${__dirname}/..`;
 const common = require("./webpack.config.js");
@@ -7,32 +9,30 @@ const common = require("./webpack.config.js");
 const cesiumSource = "node_modules/cesium/Source";
 
 module.exports = merge(common, {
-  output: {
-    filename: "js/[name].[chunkhash].js",
-    sourceMapFilename: "js/[name].[chunkhash][ext].map",
-    chunkFilename: "js/[name].[chunkhash].js",
-    assetModuleFilename: "assets/[name].[contenthash][ext]",
-    hashDigestLength: 8,
-  },
-  devtool: "source-map",
-  mode: "production",
-  target: "browserslist",
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        enforce: "pre",
-        include: path.resolve(basePath, cesiumSource),
-        sideEffects: false,
-        use: [{
-          loader: "webpack-strip-pragma-loader",
-          options: {
-            pragmas: {
-              debug: false,
-            },
-          },
-        }],
-      },
-    ],
-  },
+    output: {
+        filename: "js/[name].[chunkhash].js",
+        sourceMapFilename: "js/[name].[chunkhash][ext].map",
+        chunkFilename: "js/[name].[chunkhash].js",
+        assetModuleFilename: "assets/[name].[contenthash][ext]",
+        hashDigestLength: 8,
+    },
+    devtool: "source-map",
+    mode: "production",
+    target: "browserslist",
+    module: {
+        rules: [{
+            test: /\.js$/,
+            enforce: "pre",
+            include: path.resolve(basePath, cesiumSource),
+            sideEffects: false,
+            use: [{
+                loader: "webpack-strip-pragma-loader",
+                options: {
+                    pragmas: {
+                        debug: false,
+                    },
+                },
+            }],
+        }, ],
+    },
 });
