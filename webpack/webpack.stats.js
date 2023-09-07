b7da5d012b9bc15ba25fb82718ec8ff8ec47766b
takinekotfs <systakineko.tfs@gmail.com>
2023-09-07 21:09:12 +0700

    Update webpack.stats.js

--- webpack/webpack.stats.js
+++ webpack/webpack.stats.js
@@ -1,22 +1,26 @@
 const path = require("path");
-const { merge } = require("webpack-merge");
-const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
+const {
+    merge
+} = require("webpack-merge");
+const {
+    BundleAnalyzerPlugin
+} = require("webpack-bundle-analyzer");
 
 const basePath = `${__dirname}/..`;
 const common = require("./webpack.prod.js");
 
 module.exports = merge(common, {
-  plugins: [
-    new BundleAnalyzerPlugin({
-      analyzerMode: "static",
-      openAnalyzer: false,
-      reportFilename: path.resolve(basePath, "dist/report.html"),
-    }),
-  ],
-  devServer: {
-    hot: false,
-  },
-  optimization: {
-    chunkIds: "named",
-  },
+    plugins: [
+        new BundleAnalyzerPlugin({
+            analyzerMode: "static",
+            openAnalyzer: false,
+            reportFilename: path.resolve(basePath, "dist/report.html"),
+        }),
+    ],
+    devServer: {
+        hot: false,
+    },
+    optimization: {
+        chunkIds: "named",
+    },
 });
